ac89824bd72712becf38443c700b6040586659c4
Lorenzo D'Eri <ld@bendingspoons.com>
2022-12-20 18:50:20 +0100

    feat: add Yjs in a neanderthal way

--- client/src/Editor.tsx
+++ client/src/Editor.tsx
@@ -2,34 +2,64 @@ import { schema } from "prosemirror-schema-basic";
 import { EditorState, Transaction } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { exampleSetup } from "prosemirror-example-setup";
+import { useEffect } from "react";
+import * as Y from "yjs";
+import { WebsocketProvider } from "y-websocket";
 
-import { useEffect, useState } from "react";
+const doc = new Y.Doc();
+const wsProvider = new WebsocketProvider(
+  "ws://192.168.249.63:1234",
+  "my-roomname",
+  doc
+);
+
+wsProvider.on("status", (event: any) => {
+  console.log(event.status);
+});
 
 export const Editor = () => {
-  const [value, setValue] = useState<EditorState | undefined>(undefined);
+  // Now a simple JSON stringified version of the editor state is stored in the sharedState
+  // TODO: Figure out how to store the editor state as a Yjs type
+  const sharedState = doc.getText("sharedState");
 
   useEffect(() => {
     const state =
-      value ??
-      EditorState.create({
-        schema,
-        plugins: exampleSetup({ schema }),
-      });
+      sharedState.toString() !== ""
+        ? EditorState.fromJSON({ schema }, JSON.parse(sharedState.toString()))
+        : EditorState.create({
+            schema,
+            // plugins: exampleSetup({ schema }),
+          });
 
     const view = new EditorView(document.getElementById("editor"), {
       state,
       dispatchTransaction: (transaction: Transaction) => {
-        setValue(view.state.apply(transaction));
+        const newState = view.state.apply(transaction);
+        const jsonState = newState.toJSON();
+        sharedState.delete(0, sharedState.length);
+        sharedState.insert(0, JSON.stringify(jsonState));
+        view.updateState(newState);
       },
     });
+
+    const observe = (event: Y.YTextEvent) => {
+      if (sharedState.toString() === "") return;
+      console.log(sharedState.toString());
+      view.updateState(
+        EditorState.fromJSON({ schema }, JSON.parse(sharedState.toString()))
+      );
+    };
+
+    sharedState.observe(observe);
 
     // The focus is lost on rerender so it needs to be manually restored
     view.focus();
 
     return () => {
       view.destroy();
+      sharedState.unobserve(observe);
     };
-  }, [value]);
+  }, [sharedState]);
 
   return (
     <div>
