0fffec449d037c0ef2f4febaafd759dcc212b6ea
Lorenzo D'Eri <ld@bendingspoons.com>
2022-12-21 12:11:54 +0100

    chore: use Yjs properly for a simple text node

--- client/src/Editor.tsx
+++ client/src/Editor.tsx
@@ -7,59 +7,108 @@ import * as Y from "yjs";
 import { WebsocketProvider } from "y-websocket";
 
 const doc = new Y.Doc();
+const docState = {
+  content: doc.getText("content"),
+};
+
 const wsProvider = new WebsocketProvider(
   "ws://192.168.249.63:1234",
-  "my-roomname",
+  "Follow LorDisturbia",
   doc
 );
 
 wsProvider.on("status", (event: any) => {
-  console.log(event.status);
+  console.log(`Websocket status: ${event.status}`);
 });
 
 export const Editor = () => {
-  // Now a simple JSON stringified version of the editor state is stored in the sharedState
-  // TODO: Figure out how to store the editor state as a Yjs type
-  const sharedState = doc.getText("sharedState");
-
   useEffect(() => {
     const state =
-      sharedState.toString() !== ""
-        ? EditorState.fromJSON({ schema }, JSON.parse(sharedState.toString()))
-        : EditorState.create({
-            schema,
-            // plugins: exampleSetup({ schema }),
-          });
+      // docState.content.toString() !== ""
+      // ? EditorState.fromJSON({ schema }, JSON.parse(sharedState.toString()))
+      // :
+      EditorState.create({
+        schema,
+        plugins: exampleSetup({ schema }),
+      });
 
     const view = new EditorView(document.getElementById("editor"), {
       state,
       dispatchTransaction: (transaction: Transaction) => {
         const newState = view.state.apply(transaction);
-        const jsonState = newState.toJSON();
-        sharedState.delete(0, sharedState.length);
-        sharedState.insert(0, JSON.stringify(jsonState));
         view.updateState(newState);
+        console.log(
+          "EditorChange:",
+          "\nYjs:\t",
+          docState.content.toString() || "<empty>",
+          "\nDoc:\t",
+          view.state.doc.textContent || "<empty>"
+        );
+
+        transaction.steps.forEach((step) => {
+          const jsonStep = step.toJSON();
+          console.log(`Sending update: ${JSON.stringify(jsonStep)}`);
+          switch (jsonStep.stepType) {
+            case "replace":
+              const delta = [];
+
+              if (jsonStep.from > 0) {
+                delta.push({
+                  retain: jsonStep.from - 1,
+                });
+              }
+
+              if (jsonStep.slice && jsonStep.slice.content[0].text) {
+                delta.push({
+                  insert: jsonStep.slice.content[0].text,
+                });
+              }
+
+              if (jsonStep.to - jsonStep.from >= 0) {
+                delta.push({
+                  delete: jsonStep.to - jsonStep.from,
+                });
+              }
+
+              docState.content.applyDelta(delta);
+              break;
+          }
+        });
       },
     });
 
     const observe = (event: Y.YTextEvent) => {
-      if (sharedState.toString() === "") return;
-      console.log(sharedState.toString());
-      view.updateState(
-        EditorState.fromJSON({ schema }, JSON.parse(sharedState.toString()))
+      // if (docState.content.toString() === "") return;
+      if (docState.content.toString() === view.state.doc.textContent) return;
+
+      const transaction = view.state.tr.replaceWith(
+        0,
+        view.state.doc.content.size,
+        docState.content.toString() !== ""
+          ? [state.schema.text(docState.content.toString())]
+          : []
+      );
+      const newState = view.state.apply(transaction);
+      view.updateState(newState);
+      console.log(
+        "YjsChange:",
+        "\nYjs:\t",
+        docState.content.toString() || "<empty>",
+        "\nDoc:\t",
+        view.state.doc.textContent || "<empty>"
       );
     };
 
-    sharedState.observe(observe);
+    docState.content.observe(observe);
 
     // The focus is lost on rerender so it needs to be manually restored
     view.focus();
 
     return () => {
       view.destroy();
-      sharedState.unobserve(observe);
+      docState.content.unobserve(observe);
     };
-  }, [sharedState]);
+  }, []);
 
   return (
     <div>
